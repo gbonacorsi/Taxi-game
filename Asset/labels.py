6eb27cfa0d0302c7e84e6628ff456b7fcd65f25c
Neo <giorgio_bonacorsi@hotmail.com>
2025-09-23 09:12:10 -0400

    bulding

---
+++ Asset/labels.py
@@ -0,0 +1,8 @@
+labels={"en": {
+    "title": "Taxi Game",
+    "controls": "Controls: press w to go Up, s to go Down, a to go Left, d to go Right, e to Pick, q to Drop",
+    "elements": "Gold square = Player, Yellow Circle = Client, Orange Circle = Destination",
+    "mission": "Collect the client and deliver to destination!",
+    "score": "Score",
+    "distance_client": "Distance to Client",
+    "distance_destination": "Distance to Destination"}}
